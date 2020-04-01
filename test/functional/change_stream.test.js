b415e37623bcbd79591391e56755be4f12cc59a6
Matt Broadstone <mbroadst@mongodb.com>
2020-04-01 08:12:36 -0400

    - add `waitForStarted`, `tryNext` and `exhaust` helpers - refactor "callbacks" test to step the change stream

--- test/functional/change_stream.test.js
+++ test/functional/change_stream.test.js
@@ -13,6 +13,77 @@ const sinon = require('sinon');
 
 chai.use(require('chai-subset'));
 
+/**
+ * Waits for a change stream to start
+ *
+ * @param {ChangeStream} changeStream
+ * @param {function} callback
+ */
+function waitForStarted(changeStream, callback) {
+  changeStream.cursor.once('init', () => {
+    callback();
+  });
+}
+
+/**
+ * Iterates the next discrete batch of a change stream non-eagerly. This
+ * will return `null` if the next bach is empty, rather than waiting forever
+ * for a non-empty batch.
+ *
+ * @param {ChangeStream} changeStream
+ * @param {function} callback
+ */
+function tryNext(changeStream, callback) {
+  let complete = false;
+  function done(err, result) {
+    if (complete) return;
+
+    // if the arity is 1 then this a callback for `more`
+    if (arguments.length === 1) {
+      result = err;
+      const batch = result.cursor.firstBatch || result.cursor.nextBatch;
+      if (batch.length === 0) {
+        complete = true;
+        callback(null, null);
+      }
+
+      return;
+    }
+
+    // otherwise, this a normal response to `next`
+    complete = true;
+    changeStream.removeListener('more', done);
+    if (err) return callback(err);
+    callback(err, result);
+  }
+
+  // race the two requests
+  changeStream.next(done);
+  changeStream.cursor.once('more', done);
+}
+
+/**
+ * Exhausts a change stream aggregating all responses until the first
+ * empty batch into a returned array of events.
+ *
+ * @param {ChangeStream} changeStream
+ * @param {function} callback
+ */
+function exhaust(changeStream, bag, callback) {
+  if (typeof bag === 'function') {
+    callback = bag;
+    bag = [];
+  }
+
+  tryNext(changeStream, (err, doc) => {
+    if (err) return callback(err);
+    if (doc === null) return callback(undefined, bag);
+
+    bag.push(doc);
+    exhaust(changeStream, bag, callback);
+  });
+}
+
 // Define the pipeline processing changes
 var pipeline = [
   { $addFields: { addedField: 'This is a field added using $addFields' } },
@@ -2827,6 +2898,63 @@ describe('Change Streams', function() {
     });
   });
 
+  describe('tryNext', function() {
+    it('should return null on single iteration of empty cursor', function(done) {
+      const client = this.configuration.newClient();
+      client.connect(err => {
+        expect(err).to.not.exist;
+
+        const changeStream = client
+          .db()
+          .collection('test')
+          .watch();
+
+        tryNext(changeStream, (err, doc) => {
+          expect(err).to.not.exist;
+          expect(doc).to.not.exist;
+
+          changeStream.close(() => client.close(done));
+        });
+      });
+    });
+
+    it('should iterate a change stream until first empty batch', function(done) {
+      const client = this.configuration.newClient();
+      client.connect(err => {
+        expect(err).to.not.exist;
+
+        const collection = client.db().collection('test');
+        const changeStream = collection.watch();
+        waitForStarted(changeStream, () => {
+          collection.insertOne({ a: 42 }, err => {
+            expect(err).to.not.exist;
+
+            collection.insertOne({ b: 24 }, err => {
+              expect(err).to.not.exist;
+            });
+          });
+        });
+
+        tryNext(changeStream, (err, doc) => {
+          expect(err).to.not.exist;
+          expect(doc).to.exist;
+
+          tryNext(changeStream, (err, doc) => {
+            expect(err).to.not.exist;
+            expect(doc).to.exist;
+
+            tryNext(changeStream, (err, doc) => {
+              expect(err).to.not.exist;
+              expect(doc).to.not.exist;
+
+              changeStream.close(() => client.close(done));
+            });
+          });
+        });
+      });
+    });
+  });
+
   describe('startAfter', function() {
     let client;
     let coll;
@@ -2835,23 +2963,31 @@ describe('Change Streams', function() {
     beforeEach(function(done) {
       const configuration = this.configuration;
       client = configuration.newClient();
-      client.connect(() => {
+      client.connect(err => {
+        expect(err).to.not.exist;
+
         coll = client.db('integration_tests').collection('setupAfterTest');
-        const watch = coll.watch();
-        watch.on('change', change => {
-          if (change.operationType !== 'drop') return;
-          startAfter = change._id;
-          // console.log({ startAfter });
-        });
-        setTimeout(() =>
-          coll.insertOne({ x: 1 }, { w: 'majority', j: true }, () => {
-            coll.drop(() => {
-              coll.insertOne({ x: 2 }, { w: 'majority', j: true }, () => {
-                watch.close(done);
+        const changeStream = coll.watch();
+        waitForStarted(changeStream, () => {
+          coll.insertOne({ x: 1 }, { w: 'majority', j: true }, err => {
+            expect(err).to.not.exist;
+
+            coll.drop(err => {
+              expect(err).to.not.exist;
+
+              coll.insertOne({ x: 2 }, { w: 'majority', j: true }, err => {
+                expect(err).to.not.exist;
               });
             });
-          })
-        );
+          });
+        });
+
+        changeStream.on('change', change => {
+          if (change.operationType === 'invalidate') {
+            startAfter = change._id;
+            changeStream.close(done);
+          }
+        });
       });
     });
 
@@ -2859,7 +2995,7 @@ describe('Change Streams', function() {
       client.close(done);
     });
 
-    it.only('should work with events', {
+    it('should work with events', {
       metadata: { requires: { topology: 'replicaset', mongodb: '>=4.1.1' } },
       test: function(done) {
         const watch = coll.watch([], { startAfter });
@@ -2873,28 +3009,23 @@ describe('Change Streams', function() {
       }
     });
 
-    it.only('should work with callbacks', {
+    it('should work with callbacks', {
       metadata: { requires: { topology: 'replicaset', mongodb: '>=4.1.1' } },
-      test: function() {
-        const watch = coll.watch([], { startAfter });
-        let bucket = [];
-        function exhaust() {
-          return watch.hasNext().then(hasNext => {
-            console.log({ hasNext });
-            if (hasNext) {
-              return watch.next().then(next => {
-                bucket.push(next);
-                return exhaust();
-              });
-            } else {
-              const finalOperation = bucket.pop();
-              expect(finalOperation.operationType).to.equal('insert');
-              expect(finalOperation.fullDocument).to.include({ x: 2 });
-              return new Promise(resolve => watch.close(resolve));
-            }
+      test: function(done) {
+        const changeStream = coll.watch([], { startAfter });
+        this.defer(() => changeStream.close());
+
+        exhaust(changeStream, (err, bag) => {
+          expect(err).to.not.exist;
+
+          const finalOperation = bag.pop();
+          expect(finalOperation).to.containSubset({
+            operationType: 'insert',
+            fullDocument: { x: 2 }
           });
-        }
-        return exhaust();
+
+          done();
+        });
       }
     });
   });
